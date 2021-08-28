4c18a6c9a91be2a28899456995e0da53a547efe9
Marco Piatti <marcopiatticastro@hotmail.com>
2021-08-28 00:12:05 -0300

    Agregadas funciones para enviar y recibir paquetes enteros

--- shared/include/networking.h
+++ shared/include/networking.h
@@ -2,14 +2,81 @@
 #define NETWORKING_H_
 
 #include "serialize.h"
+#include "guards.h"
 #include <sys/socket.h>
 
 typedef enum msgHeader { HANDSHAKE, PING, ACK, DISCONNECTED } msgHeader;
 
+typedef struct packet {
+    msgHeader header;
+    t_streamBuffer* data;
+} t_packet;
+
+/**
+ * @DESC: Crea un objeto Packet en memoria
+ * @param size: tamanio alojado al stream que contiene
+ * @return t_packet*: puntero al packet creado
+ */
+t_packet* createPacket(size_t size);
+
+/**
+ * @DESC: Destruye un packet de memoria
+ * @param packet: puntero al packet a destruir
+ */
+void destroyPacket(t_packet* packet);
+
+/**
+ * @DESC: (wrapper) send sin flags con guarda
+ * @param socket: socket a enviar datos
+ * @param source: puntero a los datos enviados
+ * @param size: tamanio de los datos enviados
+ */
 void socket_send(int socket, void* source, size_t size);
 
-void socket_sendPacket(int socket, msgHeader header, streamBuffer* stream);
+/**
+ * @DESC: Envia un packet al socket
+ * 
+ * formato: [ header | tamanio del stream | stream ]
+ * 
+ * @param socket: socket a enviar el packet
+ * @param packet: puntero al packet
+ */
+void socket_sendPacket(int socket, t_packet* packet);
 
-void socket_read(int socket, void* dest, size_t size);
+/**
+ * @DESC: (wrapper) recv sin flags con guarda
+ * @param socket: socket del cual recibir los datos
+ * @param dest: puntero al cual se alojaran los datos
+ * @param size: tamanio de los datos (el puntero debe tener alojado ese tamanio minimo)
+ */
+void socket_get(int socket, void* dest, size_t size);
+
+/**
+ * @DESC: Obtiene un packet del socket
+ * @param socket: socket del cual se obtiene el packet
+ * @return t_packet*: puntero al packet obtenido
+ */
+t_packet* socket_getPacket(int socket);
+
+/**
+ * @DESC: Se conecta a un server y crea un socket
+ * @param ip: ip del server
+ * @param port: puerto del server
+ * @return int: socket del cliente conectado al server
+ */
+int connectToServer(char* serverIp, char* serverPort); //TODO Desarrollar
+
+/**
+ * @DESC: Crea un servidor para escuchar conexiones
+ * @param port: puerto del servidor
+ * @return int: socket del servidor 
+ */
+int createListenServer(char* serverPort); //TODO Desarrollar
+
+/**
+ * @DESC: queda a la escucha de nuevas conexiones
+ * @param socket: retorna un socket al primer cliente en conectarse
+ */
+void listen(int clientSocket); //TODO Desarrollar
 
 #endif // !NETWORKING_H_
--- shared/src/networking.c
+++ shared/src/networking.c
@@ -1,16 +1,37 @@
 #include "networking.h"
 
+t_packet* createPacket(size_t size){
+    t_packet* tmp = malloc(sizeof(t_packet));
+    tmp->data = createStream(size);
+    return tmp;
+}
+
+void destroyPacket(t_packet* packet){
+    destroyStream(packet->data);
+    free(packet);
+}
+
 void socket_send(int socket, void* source, size_t size){
-    send(socket, source, size, 0);
+    guard_syscall(send(socket, source, size, 0));
 }
 
-void socket_pack(int socket, msgHeader header, streamBuffer* stream){
-    streamBuffer* packet = createStream_S();
-    streamAdd_UINT8(packet, (void*)&header);
-    streamAdd_STREAM(packet, stream);
-
+void socket_sendPacket(int socket, t_packet* packet){
+    uint8_t header = packet->header;
+    socket_send(socket, packet->header, sizeof(uint8_t));
+    socket_send(socket, packet->data->offset, sizeof(uint32_t));
+    socket_send(socket, packet->data->stream, packet->data->offset);
 }
 
-void socket_read(int socket, void* dest, size_t size);
+void socket_get(int socket, void* dest, size_t size){
+    guard_syscall(recv(socket, dest, size, 0));
+}
 
-void socket_unpack(int socket, msgHeader* header, streamBuffer* stream);
+t_packet* socket_getPacket(int socket){
+    uint8_t header;
+    uint32_t streamSize;
+    socket_get(socket, &header, sizeof(uint8_t));
+    socket_get(socket, &streamSize, sizeof(uint32_t));
+    t_packet* packet = createPacket(streamSize);
+    socket_get(socket, packet->data->stream, streamSize);
+    return packet;
+}
