63dcd5491b235b696544252a478ea964bdc69dac
Marco Piatti <marcopiatticastro@hotmail.com>
2021-08-28 00:04:20 -0300

    Agregados wrappers de serializadores, simplificado createStream()

--- shared/include/serialize.h
+++ shared/include/serialize.h
@@ -9,25 +9,19 @@
 #include <commons/collections/list.h>
 #include <commons/collections/dictionary.h>
 
+#define INITIAL_STREAM_SIZE 256
+
 /**
  * @DESC: Contiene 
  *        - un stream
  *        - el tamanio de su informacion
  *        - su tamanio alojado en memoria
  */
-typedef struct streamBuffer {
+typedef struct t_streamBuffer {
     uint32_t offset;
     size_t mallocSize;
     char* stream;
-} streamBuffer;
-
-/**
- * @DESC: Crea un objeto Stream en memoria para serializar
- *        - el stream se aloca con un tamanio inicial
- *        - el offset se inicializa en 0 (ya que no se le cargo nada)
- * @return streamBuffer*: puntero al stream creado
- */
-streamBuffer* createStream_S();
+} t_streamBuffer;
 
 /**
  * @DESC: Crea un objeto Stream en memoria para deserializar
@@ -35,13 +29,15 @@ streamBuffer* createStream_S();
  *        - el offset se inicializa en 0
  * @return streamBuffer*: puntero al stream creado
  */
-streamBuffer* createStream_D(size_t size);
+t_streamBuffer* createStream(size_t size);
 
 /**
  * @DESC: Destruye un streamBuffer, y su stream, de la memoria
  * @param stream: puntero al streamBuffer a destruir
  */
-void destroyStream(streamBuffer* stream);
+void destroyStream(t_streamBuffer* stream);
+
+//----------------------------------------------------------------------//
 
 /**
  * @DESC: Agrega al stream "size" bytes desde "source"
@@ -49,35 +45,73 @@ void destroyStream(streamBuffer* stream);
  * @param source: puntero al inicio del contenido
  * @param size: tamanio de bytes a agregar
  */
-void streamAdd(streamBuffer* stream, void* source, size_t size);
+void streamAdd(t_streamBuffer* stream, void* source, size_t size);
+
+//----------------------------------------------------------------------//
 
 /**
  * @DESC: Agrega un int32_t al stream
  * @param stream: puntero al stream
  * @param source: puntero al int32_t
  */
-void streamAdd_INT32(streamBuffer* stream, void* source);
+void streamAdd_INT32_P(t_streamBuffer* stream, void* source);
+
+/**
+ * @DESC: Agrega un int32_t al stream
+ * @param stream: puntero al stream
+ * @param source: valor del int32_t
+ */
+void streamAdd_INT32(t_streamBuffer* stream, int32_t source);
+
+//----------------------------------------------------------------------//
 
 /**
  * @DESC: Agrega un uint32_t al stream
  * @param stream: puntero al stream
  * @param source: puntero al uint32_t
  */
-void streamAdd_UINT32(streamBuffer* stream, void* source);
+void streamAdd_UINT32_P(t_streamBuffer* stream, void* source);
+
+/**
+ * @DESC: Agrega un uint32_t al stream
+ * @param stream: puntero al stream
+ * @param source: valor del uint32_t
+ */
+void streamAdd_UINT32(t_streamBuffer* stream, uint32_t source);
+
+//----------------------------------------------------------------------//
 
 /**
  * @DESC: Agrega un uint8_t al stream
  * @param stream: puntero al stream
  * @param source: puntero al uint8_t
  */
-void streamAdd_UINT8(streamBuffer* stream, void* source);
+void streamAdd_UINT8_P(t_streamBuffer* stream, void* source);
+
+/**
+ * @DESC: Agrega un uint8_t al stream
+ * @param stream: puntero al stream
+ * @param source: valor del uint8_t
+ */
+void streamAdd_UINT8(t_streamBuffer* stream, uint8_t source);
+
+//----------------------------------------------------------------------//
 
 /**
  * @DESC: Agrega un string (y su tamanio antes) al stream
  * @param stream: puntero al stream
- * @param source: puntero al string (char*)
+ * @param source: puntero al string (castear a void*)
  */
-void streamAdd_STRING(streamBuffer* stream, void* source);
+void streamAdd_STRING_P(t_streamBuffer* stream, void* source);
+
+/**
+ * @DESC: Agrega un string (y su tamanio antes) al stream
+ * @param stream: puntero al stream
+ * @param source: puntero al string
+ */
+void streamAdd_STRING(t_streamBuffer* stream, char* source);
+
+//----------------------------------------------------------------------//
 
 /**
  * @DESC: Agrega la cantidad de elementos de una lista, y luego todos los elementos, al stream
@@ -85,7 +119,9 @@ void streamAdd_STRING(streamBuffer* stream, void* source);
  * @param source: puntero a la lista
  * @param streamAdd_ELEM: funcion para agregar un elemento al stream
  */
-void streamAdd_LIST(streamBuffer* stream, t_list* source, void(*streamAdd_ELEM)(streamBuffer*, void*));
+void streamAdd_LIST(t_streamBuffer* stream, t_list* source, void(*streamAdd_ELEM)(t_streamBuffer*, void*));
+
+//----------------------------------------------------------------------//
 
 /**
  * @DESC: Agrega la cantidad de elementos en el diccionario, seguido de todos los pares KEY/VALUE
@@ -93,7 +129,9 @@ void streamAdd_LIST(streamBuffer* stream, t_list* source, void(*streamAdd_ELEM)(
  * @param source: puntero al diccionario
  * @param streamAdd_VALUE: funcion para agregar un value al stream (el key se agrega solo)
  */
-void streamAdd_DICT(streamBuffer* stream, t_dictionary* source, void(*streamAdd_VALUE)(streamBuffer*, void*));
+void streamAdd_DICT(t_streamBuffer* stream, t_dictionary* source, void(*streamAdd_VALUE)(t_streamBuffer*, void*));
+
+//----------------------------------------------------------------------//
 
 /**
  * @DESC: Toma "size" bytes del stream y los guarda en memoria a partir de "dest"
@@ -101,63 +139,73 @@ void streamAdd_DICT(streamBuffer* stream, t_dictionary* source, void(*streamAdd_
  * @param dest: puntero destino pasado por referencia
  * @param size: cantidad de bytes a sacar del stream
  */
-void streamTake(streamBuffer* stream, void** dest, size_t size);
+void streamTake(t_streamBuffer* stream, void** dest, size_t size);
+
+//----------------------------------------------------------------------//
 
 /**
  * @DESC: Toma un int32_t del stream y lo copia a dest
  * @param stream: puntero al stream
  * @param dest: puntero a int32_t pasado por referencia
  */
-void streamTake_INT32_P(streamBuffer* stream, void** dest);
+void streamTake_INT32_P(t_streamBuffer* stream, void** dest);
 
 /**
  * @DESC: Retorna por valor un int32_t del stream
  * @param stream: puntero al stream
  * @return int32_t: valor del int32_t recuperado
  */
-int32_t streamTake_INT32(streamBuffer* stream);
+int32_t streamTake_INT32(t_streamBuffer* stream);
+
+//----------------------------------------------------------------------//
 
 /**
  * @DESC: Toma un uint32_t del stream y lo copia a dest
  * @param stream: puntero al stream
  * @param dest: puntero a uint32_t pasado por referencia
  */
-void streamTake_UINT32_P(streamBuffer* stream, void** dest);
+void streamTake_UINT32_P(t_streamBuffer* stream, void** dest);
 
 /**
  * @DESC: Retorna por valor un uint32_t del stream
  * @param stream: puntero al stream
  * @return int32_t: valor del uint32_t recuperado
  */
-uint32_t streamTake_UINT32(streamBuffer* stream);
+uint32_t streamTake_UINT32(t_streamBuffer* stream);
+
+//----------------------------------------------------------------------//
 
 /**
  * @DESC: Toma un uint8_t del stream y lo copia a dest
  * @param stream: puntero al stream
  * @param dest: puntero a uint8_t pasado por referencia
  */
-void streamTake_UINT8_P(streamBuffer* stream, void** dest);
+void streamTake_UINT8_P(t_streamBuffer* stream, void** dest);
 
 /**
  * @DESC: Retorna por valor un uint8_t del stream
  * @param stream: puntero al stream
  * @return int32_t: valor del uint8_t recuperado
  */
-uint8_t streamTake_UINT8(streamBuffer* stream);
+uint8_t streamTake_UINT8(t_streamBuffer* stream);
+
+//----------------------------------------------------------------------//
 
 /**
  * @DESC: Toma un string el stream (y previamente su tamanio) y lo copia a dest
  * @param stream: puntero al stream
  * @param dest: puntero al string (char*) pasado por referencia
  */
-void streamTake_STRING_P(streamBuffer* stream, void** dest);
+void streamTake_STRING_P(t_streamBuffer* stream, void** dest);
 
 /**
  * @DESC: Retorna "por valor" un string (char*) del stream
  * @param stream: puntero al stream
  * @return char*: puntero al string alojado en memoria
  */
-char* streamTake_STRING(streamBuffer* stream);
+char* streamTake_STRING(t_streamBuffer* stream);
+
+//----------------------------------------------------------------------//
 
 /**
  * @DESC: Toma del stream la cantidad de elementos de una lista, y luego todos sus elementos, y los agrega a source
@@ -165,7 +213,7 @@ char* streamTake_STRING(streamBuffer* stream);
  * @param source: puntero a la lista pasado por referencia
  * @param streamTake_ELEM: funcion para tomar un elemento del stream
  */
-void streamTake_LIST_P(streamBuffer* stream, t_list** source, void(*streamTake_ELEM_P)(streamBuffer*, void**));
+void streamTake_LIST_P(t_streamBuffer* stream, t_list** source, void(*streamTake_ELEM_P)(t_streamBuffer*, void**));
 
 /**
  * @DESC: Toma del stream una lista y retorna un puntero a esa misma
@@ -173,7 +221,9 @@ void streamTake_LIST_P(streamBuffer* stream, t_list** source, void(*streamTake_E
  * @param streamTake_ELEM_P: funcion para tomar un elemento del stream
  * @return t_list*: puntero a la lista deserializada
  */
-t_list* streamTake_LIST(streamBuffer* stream, void(*streamTake_ELEM_P)(streamBuffer*, void**));
+t_list* streamTake_LIST(t_streamBuffer* stream, void(*streamTake_ELEM_P)(t_streamBuffer*, void**));
+
+//----------------------------------------------------------------------//
 
 /**
  * @DESC: Toma del stream la cantidad de pares key/value del diccionario, y luego todos los pares key/value (en ese orden)
@@ -182,7 +232,7 @@ t_list* streamTake_LIST(streamBuffer* stream, void(*streamTake_ELEM_P)(streamBuf
  * @param source: puntero al diccionario pasado por referencia
  * @param streamTake_VALUE: funcion para tomar un value del stream (el key se recupera solo)
  */
-void streamTake_DICT_P(streamBuffer* stream, t_dictionary** source, void(*streamTake_VALUE_P)(streamBuffer*, void**));
+void streamTake_DICT_P(t_streamBuffer* stream, t_dictionary** source, void(*streamTake_VALUE_P)(t_streamBuffer*, void**));
 
 /**
  * @DESC: Toma del stream un diccionario y retorna un puntero al mismo
@@ -190,6 +240,6 @@ void streamTake_DICT_P(streamBuffer* stream, t_dictionary** source, void(*stream
  * @param streamTake_VALUE_P: funcion para tomar un value del stream (el key se recupera solo)
  * @return t_dictionary*: puntero al diccionario deserializado
  */
-t_dictionary* streamTake_DICT(streamBuffer* stream, void(*streamTake_VALUE_P)(streamBuffer*, void**));
+t_dictionary* streamTake_DICT(t_streamBuffer* stream, void(*streamTake_VALUE_P)(t_streamBuffer*, void**));
 
 #endif // !SERIALIZE_H_
--- shared/src/serialize.c
+++ shared/src/serialize.c
@@ -1,134 +1,163 @@
 #include "serialize.h"
 
-#define INITIAL_STREAM_SIZE 256
-
-streamBuffer* createStream_S(){
-    streamBuffer* tmp = malloc(sizeof(streamBuffer));
-    tmp->offset = 0;
-    tmp->mallocSize = INITIAL_STREAM_SIZE;
-    tmp->stream = malloc(tmp->mallocSize);
-    return tmp;
-}
-
-streamBuffer* createStream_D(size_t size){
-    streamBuffer* tmp = malloc(sizeof(streamBuffer));
+t_streamBuffer* createStream(size_t size){
+    t_streamBuffer* tmp = malloc(sizeof(t_streamBuffer));
     tmp->offset = 0;
     tmp->mallocSize = size;
     tmp->stream = malloc(size);
     return tmp;
 }
 
-void destroyStream(streamBuffer* stream){
+void destroyStream(t_streamBuffer* stream){
     free(stream->stream);
     free(stream);
 }
 
-void streamAssertFits(streamBuffer* stream, size_t size){
+//----------------------------------------------------------------------//
+
+void streamAssertFits(t_streamBuffer* stream, size_t size){
     while(stream->mallocSize < stream->offset + size){
         stream->mallocSize += INITIAL_STREAM_SIZE;
         stream->stream = realloc(stream->stream, stream->mallocSize);
     }
 }
 
-void streamAdd(streamBuffer* stream, void* source, size_t size){
+//----------------------------------------------------------------------//
+
+void streamAdd(t_streamBuffer* stream, void* source, size_t size){
     streamAssertFits(stream, size);
     memcpy(stream->stream + stream->offset, source, size);
     stream->offset += size;
 }
 
-void streamAdd_INT32(streamBuffer* stream, void* source){
+//----------------------------------------------------------------------//
+
+void streamAdd_INT32_P(t_streamBuffer* stream, void* source){
     streamAdd(stream, source, sizeof(int32_t));
 }
 
-void streamAdd_INT32(streamBuffer* stream, int32_t value){
-    int32_t tmp = value;
-    streamAdd_INT32(stream, (void*)&tmp);
+void streamAdd_INT32(t_streamBuffer* stream, int32_t value){
+    streamAdd_INT32_P(stream, (void*)&value);
 }
 
-void streamAdd_UINT32(streamBuffer* stream, void* source){
+//----------------------------------------------------------------------//
+
+void streamAdd_UINT32_P(t_streamBuffer* stream, void* source){
     streamAdd(stream, source, sizeof(uint32_t));
 }
 
-void streamAdd_UINT8(streamBuffer* stream, void* source){
+void streamAdd_UINT32(t_streamBuffer* stream, uint32_t value){
+    streamAdd_UINT32_P(stream, (void*)&value);
+}
+
+//----------------------------------------------------------------------//
+
+void streamAdd_UINT8_P(t_streamBuffer* stream, void* source){
     streamAdd(stream, source, sizeof(uint8_t));
 }
 
-void streamAdd_STRING(streamBuffer* stream, void* source){
+void streamAdd_UINT8(t_streamBuffer* stream, uint8_t value){
+    streamAdd_UINT8_P(stream, (void*)&value);
+}
+
+//----------------------------------------------------------------------//
+
+void streamAdd_STRING_P(t_streamBuffer* stream, void* source){
     uint32_t size = string_length(source) + 1;
-    streamAdd_UINT32(stream, &size);
+    streamAdd_UINT32(stream, size);
     streamAdd(stream, source, size);
 }
 
-void streamAdd_LIST(streamBuffer* stream, t_list* source, void(*streamAdd_ELEM)(streamBuffer*, void*)){
-    void _streamAdd_ELEM(void* elem){
-        streamAdd_ELEM(stream, elem);
-    };
-    uint32_t listSize = source->elements_count;
-    streamAdd_UINT32(stream, &listSize);
-    list_iterate(source, _streamAdd_ELEM);
+void streamAdd_STRING(t_streamBuffer* stream, char* source){
+    streamAdd_STRING(stream, (void*)source);
 }
 
-void streamAdd_DICT(streamBuffer* stream, t_dictionary* source, void(*streamAdd_VALUE_P)(streamBuffer*, void*)){
-    void _streamADD_KEYVALUE(char* key, void* value){
+//----------------------------------------------------------------------//
+
+void streamAdd_LIST(t_streamBuffer* stream, t_list* source, void(*streamAdd_ELEM_P)(t_streamBuffer*, void*)){
+    void _streamAdd_ELEM_P(void* elem){
+        streamAdd_ELEM_P(stream, elem);
+    };
+    uint32_t listSize = source->elements_count;
+    streamAdd_UINT32(stream, listSize);
+    list_iterate(source, _streamAdd_ELEM_P);
+}
+
+//----------------------------------------------------------------------//
+
+void streamAdd_DICT(t_streamBuffer* stream, t_dictionary* source, void(*streamAdd_VALUE_P)(t_streamBuffer*, void*)){
+    void _streamADD_KEYVALUE_P(char* key, void* value){
         streamAdd_STRING(stream, key);
         streamAdd_VALUE_P(stream, value);
     };
     uint32_t dictionarySize = dictionary_size(source);
-    streamAdd_UINT32(stream, &dictionarySize);
-    dictionary_iterator(source, _streamADD_KEYVALUE);
+    streamAdd_UINT32(stream, dictionarySize);
+    dictionary_iterator(source, _streamADD_KEYVALUE_P);
 }
 
-void streamTake(streamBuffer* stream, void** dest, size_t size){
+//----------------------------------------------------------------------//
+
+void streamTake(t_streamBuffer* stream, void** dest, size_t size){
     guard_nullPtr(dest, size);
     memcpy(dest, stream->stream + stream->offset, size);
     stream->offset += size;
 }
 
-void streamTake_INT32_P(streamBuffer* stream, void** dest){
+//----------------------------------------------------------------------//
+
+void streamTake_INT32_P(t_streamBuffer* stream, void** dest){
     streamTake(stream, dest, sizeof(int32_t));
 }
 
-int32_t streamTake_INT32(streamBuffer* stream){
+int32_t streamTake_INT32(t_streamBuffer* stream){
     int32_t tmp;
     int32_t* tmpPtr = &tmp;
     streamTake_INT32_P(stream, (void**)&tmpPtr);
     return tmp;
 }
 
-void streamTake_UINT32_P(streamBuffer* stream, void** dest){
+//----------------------------------------------------------------------//
+
+void streamTake_UINT32_P(t_streamBuffer* stream, void** dest){
     streamTake(stream, dest, sizeof(uint32_t));
 }
 
-uint32_t streamTake_UINT32(streamBuffer* stream){
+uint32_t streamTake_UINT32(t_streamBuffer* stream){
     uint32_t tmp;
     uint32_t* tmpPtr = &tmp;
     streamTake_UINT32_P(stream, (void**)&tmpPtr);
     return tmp;
 }
 
-void streamTake_UINT8_P(streamBuffer* stream, void** dest){
+//----------------------------------------------------------------------//
+
+void streamTake_UINT8_P(t_streamBuffer* stream, void** dest){
     streamTake(stream, dest, sizeof(uint8_t));
 }
 
-uint8_t streamTake_UINT8(streamBuffer* stream){
+uint8_t streamTake_UINT8(t_streamBuffer* stream){
     uint8_t tmp;
     uint8_t* tmpPtr = &tmp;
     streamTake_UINT8_P(stream, (void**)&tmpPtr);
     return tmp;
 }
 
-void streamTake_STRING_P(streamBuffer* stream, void** dest){
+//----------------------------------------------------------------------//
+
+void streamTake_STRING_P(t_streamBuffer* stream, void** dest){
     uint32_t size = streamTake_UINT32(stream);
     streamTake(stream, dest, size);
 }
 
-char* streamTake_STRING(streamBuffer* stream){
+char* streamTake_STRING(t_streamBuffer* stream){
     char* tmp;
     streamTake_STRING_P(stream, (void**)&tmp);
     return tmp;
 }
 
-void streamTake_LIST_P(streamBuffer* stream, t_list** source, void(*streamTake_ELEM_P)(streamBuffer*, void**)){
+//----------------------------------------------------------------------//
+
+void streamTake_LIST_P(t_streamBuffer* stream, t_list** source, void(*streamTake_ELEM_P)(t_streamBuffer*, void**)){
     guard_nullList(source);
     uint32_t listSize = streamTake_UINT32(stream);
     for (uint32_t i = 0; i < listSize; i++){
@@ -138,13 +167,15 @@ void streamTake_LIST_P(streamBuffer* stream, t_list** source, void(*streamTake_E
     }
 }
 
-t_list* streamTake_LIST(streamBuffer* stream, void(*streamTake_ELEM_P)(streamBuffer*, void**)){
+t_list* streamTake_LIST(t_streamBuffer* stream, void(*streamTake_ELEM_P)(t_streamBuffer*, void**)){
     t_list* tmpList;
     streamTake_LIST_P(stream, &tmpList, streamTake_ELEM_P);
     return tmpList;
 }
 
-void streamTake_DICT_P(streamBuffer* stream, t_dictionary** source, void(*streamTake_VALUE_P)(streamBuffer*, void**)){
+//----------------------------------------------------------------------//
+
+void streamTake_DICT_P(t_streamBuffer* stream, t_dictionary** source, void(*streamTake_VALUE_P)(t_streamBuffer*, void**)){
     guard_nullDict(source);
     uint32_t dictSize = streamTake_UINT32(stream);
     for(uint32_t i = 0; i < dictSize; i++){
@@ -155,7 +186,7 @@ void streamTake_DICT_P(streamBuffer* stream, t_dictionary** source, void(*stream
     }
 }
 
-t_dictionary* streamTake_DICT(streamBuffer* stream, void(*streamTake_VALUE_P)(streamBuffer*, void**)){
+t_dictionary* streamTake_DICT(t_streamBuffer* stream, void(*streamTake_VALUE_P)(t_streamBuffer*, void**)){
     t_dictionary* tmpDict;
     streamTake_DICT_P(stream, &tmpDict, streamTake_VALUE_P);
     return tmpDict;
