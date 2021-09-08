d0e7e878766c184bd7a5fac43080a62ff236f0ea
ucabaleiro <ucabaleiro@frba.utn.edu.ar>
2021-09-08 01:45:15 -0300

    Pequeña prueba de CSPEC

--- memory/test/test.c
+++ memory/test/test.c
@@ -1,4 +1,22 @@
 #include <cspecs/cspec.h>
+#include <stdio.h>
+#include "prueba1.h"
 
 
+context(pruebAAAA){
+
+    describe("PROBANDO"){
+
+        before{
+            printf("Antsasdasd324823794es\n");
+        }end
+        after{
+            printf("Dessasasdasdsspues\n");
+        }end
+
+        it("Deberia devolver 3."){
+            should_int(funcionPrueba()) be equal to(3);
+        }end
+    }end
+}
 
