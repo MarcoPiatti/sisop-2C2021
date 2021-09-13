a403f4d0648c72ae5904bb1c09860e584b4e3236
Marco Piatti <marcopiatticastro@hotmail.com>
2021-09-13 03:50:42 -0300

    agrego a los procesos un estimador y un tiempo de espera

--- kernel/include/process.h
+++ kernel/include/process.h
@@ -1,21 +1,26 @@
 #ifndef PROCESS_H_
 #define PROCESS_H_
 
+#include <time.h>
+
 typedef enum state { NEW, READY, BLOCKED, EXEC, SUSP_READY, SUSP_BLOCKED, TERMINATED } t_state;
 
 typedef struct process{
     int pid;
     int socket;
     t_state state;
+    double estimate;
+    time_t waitedTime;
 } t_process;
 
 /**
  * @DESC: Crea un objeto Process en memoria
  * @param id: id asignado al proceso
  * @param socket: socket con el que se conecto el proceso
+ * @param estimate: estimador inicial
  * @return t_process*: puntero al Proceso
  */
-t_process* createProcess(int id, int socket);
+t_process* createProcess(int id, int socket, double estimate);
 
 /**
  * @DESC: Destruye un Proceso de memoria
--- kernel/src/process.c
+++ kernel/src/process.c
@@ -1,13 +1,16 @@
 #include "process.h"
 
-t_process* createProcess(int id, int socket){
+t_process* createProcess(int id, int socket, double estimate){
     t_process* process = malloc(sizeof(t_process));
     process->pid = id;
     process->socket = socket;
     process->state = NEW;
+    process->estimate = estimate;
+    process->waitedTime = 0;
     return process;
 }
 
 void destroyProcess(t_process* process){
     free(process);
+    close(process->socket);
 }
