8bcb1ce466463aa34737232c74e1957d88b76d37
Marco Piatti <marcopiatticastro@hotmail.com>
2021-09-22 13:21:05 -0300

    Creo una TAD config para memoria

---
+++ memory/include/memoryConfig.h
@@ -0,0 +1,33 @@
+#ifndef MEMORYCONFIG_H_
+#define MEMORYCONFIG_H_
+
+#include <commons/config.h>
+
+typedef struct memoryConfig{
+    t_config* config;
+    char* memoryIp;
+    char* memoryPort;
+    char* swapIp;
+    char* swapPort;
+    int ramSize;
+    int pageSize;
+    char* tipoAsignacion;
+    char* algoritmoMMU;
+    int TLBSize;
+    char* algoritmoTLB;
+    int delayTLBHit;
+    int delayTLBMiss;
+    char* dumpPathTLB;
+} t_memoryConfig;
+
+/**
+ * @DESC: genera una estructura con la config de la memoria a partir de un path
+ */
+t_memoryConfig* getMemoryConfig(char* path);
+
+/**
+ * @DESC: Destruye la estructura con los datos de config
+ */
+void destroyMemoryConfig(t_memoryConfig* config);
+
+#endif // !MEMORYCONFIG_H_
---
+++ memory/src/memoryConfig.c
@@ -0,0 +1,35 @@
+#include "memoryConfig.h"
+
+t_memoryConfig* getmemoryConfig(char* path){
+    t_memoryConfig* memoryConfig = malloc(sizeof(t_memoryConfig));
+    memoryConfig->config = config_create(path);
+    memoryConfig->memoryIp = config_get_string_value(memoryConfig->config, "IP");
+    memoryConfig->memoryPort = config_get_string_value(memoryConfig->config, "PUERTO");
+    memoryConfig->swapIp = config_get_string_value(memoryConfig->config, "IP_SWAP");
+    memoryConfig->swapPort = config_get_string_value(memoryConfig->config, "PUERTO_SWAP");
+    memoryConfig->ramSize = config_get_int_value(memoryConfig->config, "TAMANIO");
+    memoryConfig->pageSize = config_get_int_value(memoryConfig->config, "TAMANIO_PAGINA");
+    memoryConfig->tipoAsignacion = config_get_string_value(memoryConfig->config, "TIPO_ASIGNACION");
+    memoryConfig->algoritmoMMU = config_get_string_value(memoryConfig->config, "ALGORITMO_REEMPLAZO_MMU");
+    memoryConfig->TLBSize = config_get_int_value(memoryConfig->config, "CANTIDAD_ENTRADAS_TLB");
+    memoryConfig->algoritmoTLB = config_get_string_value(memoryConfig->config, "ALGORITMO_REEMPLAZO_TLB");
+    memoryConfig->delayTLBHit = config_get_int_value(memoryConfig->config, "RETARDO_ACIERTO_TLB");
+    memoryConfig->delayTLBMiss = config_get_int_value(memoryConfig->config, "RETARDO_FALLO_TLB");
+    memoryConfig->dumpPathTLB = config_get_string_value(memoryConfig->config, "DUMP_PATH");
+    return memoryConfig;
+}
+
+
+
+void destroymemoryConfig(t_memoryConfig* config){
+    free(config->memoryIp);
+    free(config->memoryPort);
+    free(config->swapIp);
+    free(config->swapPort);
+    free(config->tipoAsignacion);
+    free(config->algoritmoMMU);
+    free(config->algoritmoTLB);
+    free(config->dumpPathTLB);
+    config_destroy(config->config);
+    free(config);
+}
