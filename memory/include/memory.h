f6e45413628c1acbc0049d9018ad243266b1788c
Marco Piatti <marcopiatticastro@hotmail.com>
2021-09-28 00:06:22 -0300

    Inicializacion correcta de todo lo de memoria

--- memory/include/memory.h
+++ memory/include/memory.h
@@ -4,18 +4,33 @@
 #include "networking.h"
 #include "swapInterface.h"
 #include "memoryConfig.h"
+#include "ram.h"
 #include "tlb.h"
+#include "pageTable.h"
+#include "logs.h"
 #include <string.h>
 #include <unistd.h>
-#include "logs.h"
+
+typedef struct HeapMetadata {
+    uint32_t prevAlloc;
+    uint32_t nextAlloc;
+    uint8_t isFree;
+} t_HeapMetadata;
+
 
 t_memoryConfig* memoryConfig;
 
 t_swapInterface* swapInterface;
 
-void* ram;
-
 t_TLB* tlb;
+
+t_ram* ram;
+
+t_pageTable* pageTable;
+
+//TODO: Implementar algun tipo de sincronizacion entre las TADs de ram, pageTable y TLB o por afuera.
+//Por ahora lo dejo modo croto con un mutex gigante que acapara toda la operacion de un saque.
+pthread_mutex_t mutexMemoria;
 
 extern bool (*petitionHandlers[MAX_PETITIONS])(int clientSocket, t_packet* petition);
 
--- memory/src/memory.c
+++ memory/src/memory.c
@@ -7,7 +7,11 @@ void* thread_clientHandlerFunc(void* args){
     bool keepServing = true;
     while(keepServing){
         petition = socket_getPacket(clientSocket);
+
+        pthread_mutex_lock(&mutexMemoria);
         keepServing = petitionHandlers[petition->header](clientSocket, petition);
+        pthread_mutex_unlock(&mutexMemoria);
+        
         destroyPacket(petition);
     }
     
@@ -15,37 +19,70 @@ void* thread_clientHandlerFunc(void* args){
 }
 
 int main(){
+    //Inicializa Logger
     logger = log_create("./cfg/kernel.log", "Memoria", true, LOG_LEVEL_TRACE);
     pthread_mutex_init(&mutex_log, NULL);
 
+    //Obtiene datos de config
     memoryConfig = getMemoryConfig("./cfg/memory.config");
 
-    swapHeader asign;
-    if(strcmp(memoryConfig->tipoAsignacion, "FIJA")){
-        asign = ASIGN_FIJO;
+    //Obtiene tipo de asignacion de memoria segun config
+    swapHeader asignSwap;
+    void(*MMUAssign)(t_ram*, uint32_t, int*, int*);
+    if(!strcmp(memoryConfig->tipoAsignacion, "FIJA")){
+        asignSwap = ASIGN_FIJO;
+        MMUAssign = fixed;
     }
-    else if(strcmp(memoryConfig->tipoAsignacion, "GLOBAL")){
-        asign = ASIGN_GLOBAL;
+    else if(!strcmp(memoryConfig->tipoAsignacion, "GLOBAL")){
+        asignSwap = ASIGN_GLOBAL;
+        MMUAssign = global;
     }
-    ram = malloc(memoryConfig->ramSize);
 
-    if(strcmp(memoryConfig->algoritmoTLB, "FIFO")){
-        tlb = createTLB(memoryConfig->TLBSize, fifo);
+    //Obtiene algoritmo de seleccion de victimas de MMU segun config
+    int(*MMUVictim)(t_ram*, int, int);
+    if(!strcmp(memoryConfig->algoritmoMMU, "LRU")){
+        MMUVictim = LRU;
     }
-    else if(strcmp(memoryConfig->algoritmoTLB, "LRU")){
-        tlb = createTLB(memoryConfig->TLBSize, lru);
+    else if(!strcmp(memoryConfig->algoritmoMMU, "CLOCK-M")){
+        MMUVictim = CLOCK_M;
     }
-    
 
+    //Crea interfaz al swap a partir de datos sacados de config
     swapInterface = swapInterface_create(memoryConfig->swapIp
                                         ,memoryConfig->swapPort
                                         ,memoryConfig->pageSize
-                                        ,asign);
+                                        ,asignSwap);
     
+    //Crea una ram a partir de datos sacados de config
+    ram = createRam (memoryConfig->ramSize
+                    ,memoryConfig->pageSize
+                    ,MMUVictim
+                    ,MMUAssign
+                    ,memoryConfig->maxFramesMMU);
+
+    //Obtiene algoritmo de seleccion de victimas de TLB segun config 
+    void(*TLBVictim)(t_TLB*, t_TLBEntry*);
+    if(!strcmp(memoryConfig->algoritmoTLB, "FIFO")){
+        TLBVictim = fifo;
+    }
+    else if(!strcmp(memoryConfig->algoritmoTLB, "LRU")){
+        TLBVictim = lru;
+    }
+
+    //Crea TLB a partir de datos sacados de config
+    tlb = createTLB(memoryConfig->TLBSize, TLBVictim);
+
+    //Crea tabla de paginas
+    pageTable = pageTable_create();
+
+    //Inicializo mutex gigante (DESPUES SACARLO Y HACERLO BIEN)
+    pthread_mutex_init(&mutexMemoria, NULL);
+
+    //Inicializa server y empieza a recibir clientes
     int memoryServer = createListenServer(memoryConfig->memoryIp, memoryConfig->memoryPort);
-    
     while(1){
         runListenServer(memoryServer, thread_clientHandlerFunc);
     }
+
     return 0;
 }
