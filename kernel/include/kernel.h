9fcca168e6ac7b356a5f7f79c349f2ee9c8d60b3
Matias-Rossi <matiastadeorossi@gmail.com>
2021-10-01 20:48:10 -0300

    Actualizo prototipos

--- kernel/include/kernel.h
+++ kernel/include/kernel.h
@@ -7,58 +7,20 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <unistd.h>
+#include "kernelConfig.h"
 
 #define MAX_MULTIPROCESSING 10
 
 //Colas de estado compartidas
-t_pQueue *newQueue, *readyQueue, *blockedQueue, *suspendedReadyQueue;
+t_pQueue *newQueue, *readyQueue, *blockedQueue, *suspendedReadyQueue, *suspendedBlockedQueue;
 
-pthread_t thread_longTerm, thread_mediumTerm, thread_shortTerm;
+pthread_t thread_longTerm, thread_mediumTerm;
+//Ver tema implementación shortTerm planner
 
-/**
- * @DESC: Funcion de thread que lleva procesos de new a ready
- * @param nada: obligatorio por firma, no se usa
- * @return void*: obligatorio por firma, no se usa
- */
-void* processInitializer(void* nada);
-
-/**
- * @DESC: Ejecuta las tareas de un proceso, determinados quantums
- * @param tasks: lista de tareas de un proceso
- * @param quantums: cantidad de "ciclos de cpu" que se ejecutan, si es 0 es indeterminado
- * @return int: 
- * - Retorna -1 si no hay mas tareas para procesar.
- * - Retorna 0 si la tarea a procesar es IO.
- * - Retorna 1 si quedan mas tareas CPU por procesar.
- */
-int runCPU(t_queue* tasks, int quantums);
-
-/**
- * @DESC: Funcion de thread que ejecuta rafagas de CPU de procesos
- * @param nada: obligatorio por firma, no se usa
- * @return void*: obligatorio por firma, no se usa
- */
-void* executor(void* nada);
-
-/**
- * @DESC: Ejecuta las tareas IO de un proceso, hasta llegar a una de CPU
- * @param tasks: cola de tareas de un proceso
- */
-void runIO(t_queue* tasks);
-
-/**
- * @DESC: Funcion de thread que ejecuta las rafagas IO de procesos
- * @param nada: obligatorio por firma, no se usa
- * @return void*: obligatorio por firma, no se usa
- */
-void* executorIO(void* nada);
-
-/**
- * @DESC: Crea todas las estructuras administrativas e hilos
- *        Para que empiece a funcionar el scheduler
- */
-void createScheduler();
+t_kernelConfig* config;
 
 void *auxHandler(void *vclientSocket);
+
+void *longTerm_run(void* args);
 
 #endif // !KERNEL_H_
