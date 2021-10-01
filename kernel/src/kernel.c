8711076c1a91eba78593fcd816871dffdcad2817
Matias-Rossi <matiastadeorossi@gmail.com>
2021-10-01 19:30:33 -0300

    Implementado TAD kernelConfig

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -2,15 +2,16 @@
 #include "networking.h"
 #include "commons/log.h"
 #include "commons/config.h"
+#include "kernelConfig.h"
 
-t_log *kernelLogger = log_create("./kernel.log", "KERNEL", 1, LOG_LEVEL_TRACE);
+t_log *kernelLogger;
 
 void main(void){
-    t_config *config = config_create("./kernel.cfg");
-    char *port = config_get_string_value(config, "PORT");
-    char *ip = config_get_string_value(config, "IP"); 
+    kernelLogger = log_create("./kernel.log", "KERNEL", 1, LOG_LEVEL_TRACE);
 
-    int serverSocket = createListenServer(ip, port);
+    t_kernelConfig* config = getKernelConfig("./kernel.cfg");
+
+    int serverSocket = createListenServer(config->ip, config->port);
 
     runListenServer(serverSocket, auxHandler);
 
@@ -32,76 +33,4 @@ void *auxHandler(void *vclientSocket){
         socket_sendHeader(clientSocket, OK);
         log_info(kernelLogger, "Enviado OK");
     } while (header != DISCONNECTED);
-}
-
-
-void* processInitializer(void* nada){
-    t_process* process;
-    while(1){
-        sem_wait(&sem_multiprogram);
-        if()
-            process = takeProcess(new);
-        //Realiza algun procesamiento...
-        putProcess(process, ready);
-    }
-}
-
-int runCPU(t_queue* tasks, int quantums){
-    t_task* task = queue_peek(tasks);
-    if(task == NULL) return -1;
-    if(task->isIO) return 0;
-    while(quantums){
-        sleep(QUANTUM_LENGTH);
-        task->remaining--;
-        quantums--;
-        if(task->remaining == 0){
-            task = queue_pop(tasks); 
-            free(task);
-            task = queue_peek(tasks);
-            if(task == NULL) return -1;
-            if(task->isIO) return 0;
-        }
-    }
-    return 1;
-}
-
-void* executor(void* nada){
-    while(1){
-
-    }   
-}
-
-void runIO(t_queue* tasks){
-    t_task* task = queue_peek(tasks);
-    while(task->isIO){
-        sleep(QUANTUM_LENGTH);
-        task->remaining--;
-        if(task->remaining == 0){
-            task = queue_pop(tasks);
-            free(task);
-            task = queue_peek(tasks);
-        }
-    }
-}
-
-void* executorIO(void* nada){
-    while(1){
-        t_process* process = takeProcess(blocked);
-        runIO(process->tasks);
-        putProcess(process, ready);
-    }
-}
-
-void createScheduler(){
-    new = createpQueue();
-    ready = createpQueue();
-    blocked = createpQueue();
-    pthread_create(&thread_processInitializer, NULL, processInitializer, NULL);
-    pthread_detach(thread_processInitializer);
-    pthread_create(&thread_executorIO, NULL, executorIO, NULL);
-    pthread_detach(thread_executorIO);
-    for (int i = 0; i < MAX_MULTIPROCESSING; i++){
-        pthread_create(&thread_executor[i], NULL, executor, NULL);
-        pthread_detach(thread_executor[i]);
-    }
 }
