59ccf8c9f9a18b90cb2957f4832f9d9616ecc879
ucabaleiro <ucabaleiro@frba.utn.edu.ar>
2021-10-05 18:08:29 -0300

    Agregado t_heapMetadata

--- memory/include/memory.h
+++ memory/include/memory.h
@@ -1,7 +1,16 @@
 #ifndef MEMORY_H_
 #define MEMORY_H_
+#include <stdint.h>
 
 void *auxHandler(void *vclientSocket);
+
+typedef​ ​struct​ ​heapMetadata​ { 
+    uint32_t prevAlloc,
+    uint32_t nextAlloc,
+    uint8_t isFree,
+} t_heapMetadata;
+
+
 
 #endif // !MEMORY_H_
 
