b70d62b5d590bd24d48193ba004de91250edbb6c
Marco Piatti <marcopiatticastro@hotmail.com>
2021-11-01 22:40:58 -0300

    cambio carpincho memoryUsage, chequea integridad de dato pequeño

--- carpinchos/memoryUsage/memoryUsage.c
+++ carpinchos/memoryUsage/memoryUsage.c
@@ -1,25 +1,28 @@
 #include <pthread.h>
 #include "matelib.h"
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 #define CONFIG "./memoryUsage/memoryUsage.config"
 
-//Ajustar config de kernel para que grado de multiprog < THREAD_COUNT
+// Carpincho para asertar integridad de datos en alloc, write, read, free
+// Se le manda la cadena "Hola :)" y se la lee con otra variable para contrastar que sean iguales.
 
 int main(){
     mate_instance mate;
     mate_init(&mate, CONFIG);
 
-    mate_pointer puntero = mate_memalloc(&mate, 1020);
+    mate_pointer puntero = mate_memalloc(&mate, 8);
     
     char* mensaje = "Hola :)";
-    mate_memwrite(&mate, mensaje, puntero+900, 8);
+    printf("%s\n", mensaje);
+    mate_memwrite(&mate, mensaje, puntero, 8);
 
-    int algunValor;
-    mate_memread(&mate, puntero+100, &algunValor, 4);
-
+    char* mensajeLeido = malloc(8);
+    mate_memread(&mate, puntero, mensajeLeido, 8);
     mate_memfree(&mate, puntero);
-
     mate_close(&mate);
+    printf("%s\n", mensajeLeido);
 }
