1b335267c8b95dc1577a9d1e27fd961ad5ffaac6
ucabaleiro <ucabaleiro@frba.utn.edu.ar>
2021-11-20 18:37:58 -0300

    Anado archivo para utils genericas

---
+++ shared/include/utils.h
@@ -0,0 +1,8 @@
+#ifndef SHARED_UTILS_H
+#define SHARED_UTILS_H
+#include <stdint.h>
+
+int32_t min(int32_t a, int32_t b);
+
+
+#endif // !SHARED_UTILS_H
---
+++ shared/src/utils.c
@@ -0,0 +1,6 @@
+#include "utils.h"
+
+int32_t min(int32_t a, int32_t b){
+    if (a < b) return a;
+    return b;
+}
