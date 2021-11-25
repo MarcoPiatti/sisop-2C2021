10cb3cb2094007a956f89dd7489ca2f0551816b6
ucabaleiro <ucabaleiro@frba.utn.edu.ar>
2021-11-24 21:44:05 -0300

    Orden + elimino headers de funciones no usadas.

--- memory/include/memory.h
+++ memory/include/memory.h
@@ -13,18 +13,23 @@
 pthread_mutex_t logMut;
 pthread_mutex_t ramMut;
 pthread_mutex_t metadataMut;
-
 void initalizeMemMutex();
 void destroyMemMutex();
 
 t_swapInterface* swapInterface;
-void* auxHandler(void *vclientSocket);
+t_dictionary *pageTables;
+t_memoryConfig *config;
+t_log *memLogger;
+
+extern bool (*petitionHandlers[MAX_PETITIONS])(t_packet* petition, int socket);
+
+void *petitionHandler(void *_clientSocket);
 
 // Algoritmo (clock-m o LRU) toma un frame de inicio y un frame final y eligen la victima dentro del rango.
 uint32_t (*algoritmo)(uint32_t start, uint32_t end);
 
 // Asignacion fija o global, devuelven en los parametros un rango de frames entre los cuales se puede elegir una victima.
-void (*assignacion)(uint32_t *start, uint32_t *end, uint32_t PID);
+void (*asignacion)(uint32_t *start, uint32_t *end, uint32_t PID);
 
 typedef struct heapMetadata { 
     uint32_t prevAlloc;
@@ -46,42 +51,30 @@ typedef struct frameMetadata {
 
 typedef struct memoryMetadata{
     uint32_t entryQty;
+    uint32_t *firstFrame; // Array de PIDS donde el indice es el numero de "bloque" asignado a un carpincho en asig fija.
     uint32_t counter;
     t_frameMetadata **entries;
 } t_memoryMetadata;
 
-extern bool (*petitionHandlers[MAX_PETITIONS])(t_packet* petition, int socket);
-
-t_log *logger;
 t_memory *ram;
-t_dictionary *pageTables;
-t_memoryConfig *config;
 t_memoryMetadata *metadata;
 
-
 t_memory *initializeMemory(t_memoryConfig *config);
+void destroyMemory(t_memory* mem);
 
 t_memoryMetadata *initializeMemoryMetadata(t_memoryConfig *config);
-
-void *petitionHandler(void *_clientSocket);
-
 void destroyMemoryMetadata(t_memoryMetadata *metadata);
 
-void memread(uint32_t bytes, uint32_t address, int PID, void *destination);
-
-void memwrite(uint32_t bytes, uint32_t address, int PID, void *from);
-
-int32_t getFreeFrame(uint32_t start, uint32_t end);
-
+// Utils
 int32_t getPage(uint32_t address, t_memoryConfig *cfg);
-
 int32_t getOffset(uint32_t address, t_memoryConfig *cfg);
+bool isFree(uint32_t frame);
+uint32_t getFrameTimestamp(uint32_t frame);
 
-int32_t getFrame(uint32_t PID, uint32_t page);
 
 // Asignacion:
 void fijo(int32_t *start, int32_t *end, uint32_t PID);
-void global(int32_t *start, int32_t *end, uint32_t PID);
+void global(int32_t *start, int32_t *end, uint32_t PID); // Param PID no se usa, pero es necesario para cumplir con interfaz.
 
 /**
  * @DESC: Devuelve un puntero al frame pedido.
@@ -90,29 +83,25 @@ void global(int32_t *start, int32_t *end, uint32_t PID);
  * @return void*: puntero al frame requerido.
  */
 void *ram_getFrame(t_memory* ram, uint32_t frameN);
-
-uint32_t replace(uint32_t victim, uint32_t PID, uint32_t page);
-
-bool isFree(uint32_t frame);
-uint32_t getFrameTimestamp(uint32_t frame);
-
-uint32_t LRU(uint32_t start, uint32_t end);
-
 void ram_editFrame(t_memory *mem, uint32_t offset, uint32_t frame, void *from, uint32_t size);
+void readFrame(t_memory *mem, uint32_t frame, void *dest);
+void writeFrame(t_memory *mem, uint32_t frame, void *from);
+
 
 /**
- * @DESC: trae una pagina de swap realizando los reemplazos correspondientes. A IMPLEMENTAR.
+ * @DESC: trae una pagina de swap realizando los reemplazos correspondientes.
  * @param PID: PID del procesos solicitante.
  * @param pageN: pagina solicitada.
  * @return uint32_t: numero de frame en el que quedo la pagina.
  */
 uint32_t swapPage(uint32_t PID, uint32_t pageN);
+int32_t getFreeFrame(uint32_t start, uint32_t end);
+int32_t getFrame(uint32_t PID, uint32_t page);
+uint32_t replace(uint32_t victim, uint32_t PID, uint32_t page);
 
-// TODO: Implementar algorimos clock-m:
+// TODO: Implementar algorimo clock-m:
 uint32_t clock_m(uint32_t start, uint32_t end);
 uint32_t LRU(uint32_t start, uint32_t end);
-
-
 
 #endif // !MEMORY_H_
 
