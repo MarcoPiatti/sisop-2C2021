5da0fd5bebdbcb356d4212260481786a26006e57
Marco Piatti <marcopiatticastro@hotmail.com>
2021-11-28 19:31:26 -0300

    dejo adelantado petitionHandlers de swap

--- swamp/src/petitionHandlers.c
+++ swamp/src/petitionHandlers.c
@@ -1,7 +1,19 @@
 #include "swap.h"
 
 void savePage(t_packet *received, int clientSocket){
-    ;
+    uint32_t pid = streamTake_UINT32(petition->payload);
+    int32_t pageNumber = streamTake_INT32(petition->payload);
+    void* pageData = NULL;
+    streamTake(petition->payload, &pageData, (size_t)swapConfig->pageSize);
+
+    bool rc = asignacion(pid, pageNumber, pageData);
+    
+    t_packet* response;
+    if(rc) response = createPacket(OK_MEM, 0);
+    else response = createPacket(ERROR_MEM, 0);
+    socket_sendPacket(memorySocket, response);
+    destroyPacket(response);
+    free(pageData);
 }
 
 void readPage(t_packet *received, int clientSocket){
@@ -13,7 +25,9 @@ void destroyPage(t_packet *received, int clientSocket){
 }
 
 void memDisconnect(t_packet *received, int clientSocket){
-    ;
+    t_packet* response = createPacket(OK_MEM, 0);
+    socket_sendPacket(memorySocket, response);
+    destroyPacket(response);
 }
 
 void (*petitionHandler[MAX_MEM_PETITIONS])(t_packet *received, int clientSocket) = {
