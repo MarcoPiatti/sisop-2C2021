0b001ac96ca8985caaaad028889cb96add36714d
Marco Piatti <marcopiatticastro@hotmail.com>
2021-11-28 19:19:20 -0300

    cambios menores en main de swap

--- swamp/include/swap.h
+++ swamp/include/swap.h
@@ -1,18 +1,33 @@
 #ifndef SWAP_H_
 #define SWAP_H_
-#include "networking.h"
-#include "commons/log.h"
-#include "commons/config.h"
+
 #include "swapConfig.h"
+#include <commons/collections/list.h>
+#include <stdint.h>
+#include "networking.h"
 #include "swapFile.h"
-#include "sleeper.h"
+#include "logs.h"
 
 t_swapConfig* swapConfig;
 
-t_log *swapLogger;
+// Lista de archivos de swap usados
+t_list* swapFiles;
 
-int (*asignacion) (int PID, int page, void* content);
+/**
+ * @DESC: Revisa si un PID existe entre todos los archivos del swap
+ * @param pid: PID buscado
+ * @return t_swapFile*: puntero al swapFile que tiene al PID, NULL si no hay ninguno
+ */
+t_swapFile* pidExists(uint32_t pid);
 
-extern void (*petitionHandler[MAX_MEM_PETITIONS])(t_packet *received, int clientSocket);
+/**
+ * @DESC: Puntero a funcion usado para elegir el modo de asignacion (fija/global)
+ */
+bool (*asignacion)(uint32_t pid, int32_t page, void* pageContent);
+
+/**
+ * @DESC: Funciones con respuestas a peticiones de memoria, definidas en swapHandler.c
+ */
+extern void (*swapHandler[MAX_MEM_PETITIONS])(t_packet* petition, int memorySocket);
 
 #endif // !SWAP_H_
--- swamp/src/swap.c
+++ swamp/src/swap.c
@@ -1,39 +1,39 @@
 #include "swap.h"
 
 
-void main(void){
-    swapLogger = log_create("./swap.log", "SWAP", 1, LOG_LEVEL_TRACE);
+int main(){
+    logger = log_create("./cfg/swamp.log", "Swap", true, LOG_LEVEL_TRACE);
+    pthread_mutex_init(&mutex_log, NULL);
 
-    t_swapConfig *swapConfig = getswapConfig("./swap.cfg");
+    swapConfig = getswapConfig("./cfg/swamp.config");
+    swapFiles = list_create();
+    for(int i = 0; swapConfig->swapFiles[i]; i++)
+        list_add(swapFiles, swapFile_create(swapConfig->swapFiles[i], swapConfig->fileSize, swapConfig->pageSize));
 
-    t_list *swapFiles = list_create();
-    for(int i = 0; swapConfig->swapFiles[i]; i++){
-        list_add(swapFiles, swapFile_create(swapConfig->swapFiles[i],
-        swapConfig->fileSize,
-        swapConfig->pageSize));
-    } 
-    
     int serverSocket = createListenServer(swapConfig->swapIP, swapConfig->swapPort);
-    int clientSocket = getNewClient(serverSocket);
+    while(1){
+        int memorySocket = getNewClient(serverSocket);
+        swapHeader asignType = socket_getHeader(memorySocket);
+        if (asignType == ASIGN_FIJO) asignacion = fija;
+        else if (asignType == ASIGN_GLOBAL) asignacion = global;
 
-    int header = socket_getHeader(clientSocket);
-
-    if (header == ASIG_FIJA){
-        asignacion = fija;
+        t_packet* petition;
+        while(1){
+            petition = socket_getPacket(memorySocket);
+            if(petition == NULL){
+                if(!retry_getPacket(memorySocket, &petition)){
+                    close(memorySocket);
+                    break;
+                }
+            }
+            for(int i = 0; i < swapConfig->delay; i++){
+                usleep(1000);
+            }
+            swapHandler[petition->header](petition, memorySocket);
+            destroyPacket(petition);
+        }
     }
-    if (header == ASIG_GLOBAL){
-        asignacion = global;
-    }
-
-    t_packet *receivedPetition;
-
-    while (1){
-        receivedPetition = socket_getPacket(clientSocket);
-        milliSleep(swapConfig->delay);
-        petitionHandler[receivedPetition->header](receivedPetition, clientSocket);
-        destroyPacket(receivedPetition);
-    }
-    
+    return 0;
 }
 
 int fija(int PID, int PAGE, void *content){
