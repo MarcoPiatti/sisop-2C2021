97422899d71486e64f7993fa239b99cf85846e39
ucabaleiro <ucabaleiro@frba.utn.edu.ar>
2021-12-08 23:36:57 -0300

    typo rompedora parte 2

--- memory/src/memoryConfig.c
+++ memory/src/memoryConfig.c
@@ -24,7 +24,7 @@ t_memoryConfig *getMemoryConfig(char *path){
     memConfig -> TLBReplacementAlgorithm = config_get_string_value(memConfig-> config, "ALGORITMO_REEMPLAZO_TLB");
     memConfig -> TLBHitDelay = config_get_int_value(memConfig-> config, "RETARDO_ACIERTO_TLB");
     memConfig -> TLBMissDelay = config_get_int_value(memConfig-> config, "RETARDO_FALLO_TLB");
-    memConfig -> TLBPathDump = config_get_string_value(memConfig-> config, "PATH_DUMP_TLB");
+    memConfig -> TLBPathDump = config_get_string_value(memConfig-> config, "DUMP_PATH");
 
     return memConfig;
 }   
