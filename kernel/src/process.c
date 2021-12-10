b3fb17bad58fe998116f2f0291af252859eef527
ucabaleiro <ucabaleiro@frba.utn.edu.ar>
2021-12-10 02:18:06 -0300

    destroyProcess cierra socket + cambios en la TAD

--- kernel/src/process.c
+++ kernel/src/process.c
@@ -1,19 +1,20 @@
 #include "process.h"
+#include <stdlib.h>
+#include <unistd.h>
 
-
-t_process* createProcess(uint32_t id, int socket, double estimator){
+t_process* createProcess(int id, int socket, double estimate){
     t_process* process = malloc(sizeof(t_process));
     process->pid = id;
     process->socket = socket;
     process->state = NEW;
-    process->estimator = estimator;
-    process->waited = 0;
-    clock_gettime(CLOCK_MONOTONIC, &process->startTime);
-
+    process->estimate = estimate;
+    process->waitedTime = 0;
+    process->inited = false;
     return process;
 }
 
 void destroyProcess(t_process* process){
+    close(process->socket);
     free(process);
 }
 
