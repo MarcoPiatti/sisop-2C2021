1013f76b2664d6bb54f7b05f943a92e1e7ca7681
Marco Piatti <marcopiatticastro@hotmail.com>
2021-12-10 02:18:13 -0300

    rework tad semaforo para que sea mas parecido a iodevice

--- kernel/include/mateSem.h
+++ kernel/include/mateSem.h
@@ -5,29 +5,22 @@
 #include "pQueue.h"
 #include "process.h"
 
-//Todo: actualizar comentarios de documentacion
-
-typedef enum processState {CONTINUE, BLOCK, EXIT} processState;
-
-typedef struct t_processQueues {
-    t_pQueue *newQueue;
-    t_pQueue *readyQueue;
-    t_pQueue *blockedQueue;
-    t_pQueue *suspendedReadyQueue;
-    t_pQueue *suspendedBlockedQueue;
-    t_pQueue *execQueue;
-} t_processQueues;
-
 /**
  * @DESC: Semaforo para carpinchos.
- *        tiene un nombre, y un semaforo interno con el cual gestiona su contador.
- *        tiene una cola de procesos en espera (que hicieron mateSem_wait).
- *        tiene un thread que con cada post debe tomar un proceso de la cola y lo pone en ready
+ * - nombre: nombre.
+ * - sem: contador del semaforo.
+ * - sem_cond: condition variable para despertar al semaforo cuando tenga que pasar alguien
+ * - sem_mutex: mutex de la contidion variable
+ * - waitingProcesses: cola de procesos que esperan a usar al semaforo
+ * - thread_mateSem: hilo que hara de semaforo, dejando pasar procesos
  */
 typedef struct mateSem {
     char* nombre;
-    int semaforo;
+    unsigned int sem;
+    pthread_cond_t sem_cond;
+    pthread_mutex_t sem_mutex;
     t_pQueue* waitingProcesses;
+    pthread_t thread_mateSem;
 } t_mateSem;
 
 /**
@@ -35,12 +28,9 @@ typedef struct mateSem {
  * @param nombre: nombre del mateSem
  * @param contadorInicial: valor inicial del contador.
  * @param mateSemFunc: funcion que ejecutara el thread del mateSem.
- *                     Debe tomar por argumento un puntero a mateSem, para tener una referencia de si mismo.
- *                     Debe hacer un wait del semaforo interno cada vez que tome un proceso.
- *                     Y luego meterlo a una cola de ready, o similar.
  * @return t_mateSem*: puntero al struct del mateSem creado
  */
-t_mateSem* mateSem_create(char* nombre, unsigned int contadorInicial);
+t_mateSem* mateSem_create(char* nombre, unsigned int contadorInicial, void* (* mateSemFunc)(void*));
 
 /**
  * @DESC: Destruye un mateSem, su thread, su cola de espera, y su semaforo.
@@ -54,13 +44,12 @@ void mateSem_destroy(t_mateSem* mateSem);
  * @param mateSem: el mateSem usado
  * @param process: proceso (carpincho) que espera
  */
-processState mateSem_wait(t_mateSem* mateSem, t_process* process, t_processQueues direcciones);
+bool mateSem_wait(t_mateSem* mateSem, t_process* process);
 
 /**
  * @DESC: Incrementa el contador del mateSem en 1
  * @param mateSem: mateSem en cuestion
  */
-void mateSem_post(t_mateSem* mateSem, t_processQueues direcciones);
-
+void mateSem_post(t_mateSem* mateSem);
 
 #endif // !MATESEM_H_
