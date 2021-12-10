c0ec31c184bfb905a9ffbae97432d7c7f1a489d0
Matias-Rossi <matiastadeorossi@gmail.com>
2021-12-09 21:54:57 -0300

    Arreglo lib temporal.h :D

--- memory/include/tlb.h
+++ memory/include/tlb.h
@@ -2,7 +2,7 @@
 #include<commons/collections/queue.h>
 #include<commons/collections/dictionary.h>
 #include<pthread.h>
-#include<commons/time.h>
+#include<commons/temporal.h>
 #include<stdbool.h>
 #include<stdio.h>
 #include<stdlib.h>
